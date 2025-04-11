36e14464c6b79b79360c546c6692aa91b562949a
greenbird90 <saripudin.e2@gmail.com>
2025-04-11 16:51:26 +0700

    revisi

--- ors_config.py
+++ ors_config.py
@@ -1,7 +1,7 @@
+import os
 
-API_KEY = "5b3ce3597851110001cf624871410763ecec4c0fa324c8d35f99fbda"
-
+API_KEY = os.getenv("ORS_API_KEY")
 COORDINATES = [
-    [106.817375, -6.230952],   # Kantor Telkomsel
-    [106.730059, -6.351330]    # Pamulang Barat
+    [106.817375, -6.230952],  # Kantor Telkomsel
+    [106.730059, -6.351330]   # Pamulang Barat
 ]
